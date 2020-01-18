296f5d30eadb3dc5131b4d8e858e00b70551b70a
Orange <1036594618@qq.com>
2020-01-19 02:00:27 +0800

    UserController 层

---
+++ gmall-user/src/main/java/com/orange/gmall/gmalluser/service/UserService.java
@@ -0,0 +1,4 @@
+package com.orange.gmall.gmalluser.service;
+
+public interface UserService {
+}
---
+++ gmall-user/src/main/java/com/orange/gmall/gmalluser/service/impl/UserServiceImpl.java
@@ -0,0 +1,6 @@
+package com.orange.gmall.gmalluser.service.impl;
+
+import com.orange.gmall.gmalluser.service.UserService;
+
+public class UserServiceImpl implements UserService {
+}
