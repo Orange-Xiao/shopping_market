7332161d99d695ffe75cea177a8bf2f5f5883818
Orange <1036594618@qq.com>
2020-01-20 00:05:49 +0800

    2020 1/20 Night

---
+++ gmall-user/src/main/java/com/orange/gmall/gmalluser/mapper/UmsMemberReceiveAddressMapper.java
@@ -0,0 +1,8 @@
+package com.orange.gmall.gmalluser.mapper;
+
+import com.orange.gmall.gmalluser.dao.UmsMemberReceiveAddress;
+import tk.mybatis.mapper.common.Mapper;
+
+public interface UmsMemberReceiveAddressMapper extends Mapper<UmsMemberReceiveAddress> {
+
+}
