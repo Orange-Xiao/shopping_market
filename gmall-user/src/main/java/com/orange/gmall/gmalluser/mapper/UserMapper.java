57cd1c43324bab49e6f88628303777f26fbb46cf
Orange <1036594618@qq.com>
2020-01-20 00:06:05 +0800

    2020 1/20 Night

--- gmall-user/src/main/java/com/orange/gmall/gmalluser/mapper/UserMapper.java
+++ gmall-user/src/main/java/com/orange/gmall/gmalluser/mapper/UserMapper.java
@@ -1,4 +1,10 @@
 package com.orange.gmall.gmalluser.mapper;
 
-public class UserMapper {
+import com.orange.gmall.gmalluser.dao.UmsMember;
+import tk.mybatis.mapper.common.Mapper;
+
+import java.util.List;
+
+public interface UserMapper extends Mapper<UmsMember> {
+    List<UmsMember> selectAll();
 }
