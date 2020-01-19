418d60e843a52039c9fbbdeffd3c1049e9772f6d
Orange <1036594618@qq.com>
2020-01-20 00:05:11 +0800

    2020 1/20 Night

--- gmall-user/src/main/java/com/orange/gmall/gmalluser/controller/UserController.java
+++ gmall-user/src/main/java/com/orange/gmall/gmalluser/controller/UserController.java
@@ -1,4 +1,38 @@
 package com.orange.gmall.gmalluser.controller;
 
+import com.orange.gmall.gmalluser.dao.UmsMember;
+import com.orange.gmall.gmalluser.dao.UmsMemberReceiveAddress;
+import com.orange.gmall.gmalluser.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
+
+@Controller
 public class UserController {
+    @Autowired
+    UserService userService;
+
+    @RequestMapping("getReceiveAddressByMemberId")
+    @ResponseBody
+    public List<UmsMemberReceiveAddress> getReceiveAddressByMemberId(String memberId){
+        List<UmsMemberReceiveAddress> umsMemberReceiveAddresses = userService.getReceiveAddressByMemberId(memberId);
+        return umsMemberReceiveAddresses;
+    }
+
+
+    @RequestMapping("getAllUser")
+    @ResponseBody
+    public List<UmsMember> getAllUser(){
+        List<UmsMember> umsMembers = userService.getAllUser();
+        return umsMembers;
+    }
+
+    @RequestMapping("index")
+    @ResponseBody
+    public String index(){
+        return "hello user";
+    }
 }
