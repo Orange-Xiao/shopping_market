6ff3a7ce4a312e56b17f20e41f572109306755ef
Orange <1036594618@qq.com>
2020-01-20 00:06:21 +0800

    2020 1/20 Night

--- gmall-user/src/main/java/com/orange/gmall/gmalluser/service/impl/UserServiceImpl.java
+++ gmall-user/src/main/java/com/orange/gmall/gmalluser/service/impl/UserServiceImpl.java
@@ -1,6 +1,44 @@
 package com.orange.gmall.gmalluser.service.impl;
 
+import com.orange.gmall.gmalluser.dao.UmsMember;
+import com.orange.gmall.gmalluser.dao.UmsMemberReceiveAddress;
+import com.orange.gmall.gmalluser.mapper.UmsMemberReceiveAddressMapper;
+import com.orange.gmall.gmalluser.mapper.UserMapper;
 import com.orange.gmall.gmalluser.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import java.util.List;
+
+@Service
 public class UserServiceImpl implements UserService {
+    @Autowired
+    UserMapper userMapper;
+
+    @Autowired
+    UmsMemberReceiveAddressMapper umsMemberReceiveAddressMapper;
+
+    @Override
+    public List<UmsMember> getAllUser() {
+
+        List<UmsMember> umsMemberList = userMapper.selectAll();//userMapper.selectAllUser();
+
+        return umsMemberList;
+    }
+
+    @Override
+    public List<UmsMemberReceiveAddress> getReceiveAddressByMemberId(String memberId) {
+
+        // 封装的参数对象
+        UmsMemberReceiveAddress umsMemberReceiveAddress = new UmsMemberReceiveAddress();
+        umsMemberReceiveAddress.setMemberId(memberId);
+        List<UmsMemberReceiveAddress> umsMemberReceiveAddresses = umsMemberReceiveAddressMapper.select(umsMemberReceiveAddress);
+
+
+//        Example example = new Example(UmsMemberReceiveAddress.class);
+//        example.createCriteria().andEqualTo("memberId",memberId);
+//        List<UmsMemberReceiveAddress> umsMemberReceiveAddresses = umsMemberReceiveAddressMapper.selectByExample(example);
+
+        return umsMemberReceiveAddresses;
+    }
 }
