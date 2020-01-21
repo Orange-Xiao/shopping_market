33dcf8b78f0982927cd2e5f848601089f2f8a49d
Orange <1036594618@qq.com>
2020-01-22 01:15:46 +0800

    2020 1/22 环境终于Ok

--- gmall-user/src/main/java/com/orange/gmall/gmalluser/bean/UmsMember.java
+++ gmall-user/src/main/java/com/orange/gmall/gmalluser/bean/UmsMember.java
@@ -1,9 +1,8 @@
 package com.orange.gmall.gmalluser.bean;
 
-import org.springframework.data.annotation.Id;
-
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.util.Date;
 
 /**
--- gmall-user/src/main/java/com/orange/gmall/gmalluser/mapper/UserMapper.java
+++ gmall-user/src/main/java/com/orange/gmall/gmalluser/mapper/UserMapper.java
@@ -6,5 +6,5 @@ import tk.mybatis.mapper.common.Mapper;
 import java.util.List;
 
 public interface UserMapper extends Mapper<UmsMember> {
-    List<UmsMember> selectAll();
+    List<UmsMember> selectAllUser();
 }
