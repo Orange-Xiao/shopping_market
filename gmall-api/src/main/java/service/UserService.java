3de360167d1bed3274ea43b54651ed85f929027a
Orange <1036594618@qq.com>
2020-01-25 20:23:46 +0800

    2020 1/25 N

---
+++ gmall-api/src/main/java/service/UserService.java
@@ -0,0 +1,14 @@
+package service;
+
+
+import bean.UmsMember;
+import bean.UmsMemberReceiveAddress;
+
+import java.util.List;
+
+public interface UserService {
+
+    List<UmsMember> getAllUser();
+
+    List<UmsMemberReceiveAddress> getReceiveAddressByMemberId(String memberId);
+}
